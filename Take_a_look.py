9dae4b504cd49800e3d4b4506f03302ce163e772
Sept_C <774932729@qq.com>
2020-03-21 20:32:02 +0800

    add ppt

--- Take_a_look.py
+++ Take_a_look.py
@@ -3,8 +3,17 @@ import argparse
 
 def take_a_look(data_file):
     with open(data_file,'r') as reader:
-        for line in reader.readlines():
-            print(line)
+        data = json.load(reader)
+        article_count = 0
+        question_count = 0
+        article_len = 0
+        for article in data['data']:
+            article_count += 1
+            for paragraph in article['paragraphs']:
+                article_len += len(paragraph['context'])
+                for query in paragraph['qas']:
+                    question_count += 1
+        return article_len/article_count,question_count,article_count
 
 
 
@@ -13,7 +22,10 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_file',required=True,help='the file path that you want take a look')
     args = parser.parse_args()
-    take_a_look(args.data_file)
+    avg_article_len,question_count,article_count = take_a_look(args.data_file)
+    print('article average len: %f' % avg_article_len)
+    print('article_count: %d' % article_count)
+    print('question_count: %d' % question_count)
 
 
 
